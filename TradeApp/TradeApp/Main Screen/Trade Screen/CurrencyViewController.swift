65a73d6750b4f8ca3c08b019dd26f321f5b00b18
Andrey Prez <aw06.1994@yandex.ru>
2023-05-13 19:37:14 +0300

    Screens are added + Storyboard

---
+++ TradeApp/TradeApp/Main Screen/Trade Screen/CurrencyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyViewController.swift
+//  TradeApp
+//
+//  Created by Андрей  on 13.05.2023.
+//
+
+import UIKit
+
+class CurrencyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ TradeApp/TradeApp/Main Screen/Trade Screen/TradeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TradeViewController.swift
+//  TradeApp
+//
+//  Created by Андрей  on 13.05.2023.
+//
+
+import UIKit
+
+class TradeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
